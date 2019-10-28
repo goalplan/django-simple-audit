0af15f38e58279188e11e2520820d24dc57e7e3f
Lauren Rouse <lauren.l.rouse@gmail.com>
2019-10-28 12:40:39 -0500

    Removing call on is_anonymous property, and black reformatting

--- simple_audit/middleware.py
+++ simple_audit/middleware.py
@@ -4,28 +4,31 @@ from django.utils.deprecation import MiddlewareMixin
 from .models import AuditRequest
 from . import settings
 
+
 class TrackingRequestOnThreadLocalMiddleware(MiddlewareMixin):
     """Middleware that gets various objects from the
     request object and saves them in thread local storage."""
 
     def _get_ip(self, request):
         # get real ip
-        if 'HTTP_X_FORWARDED_FOR' in request.META:
-            ip = request.META['HTTP_X_FORWARDED_FOR']
-        elif 'Client-IP' in request.META:
-            ip = request.META['Client-IP']
+        if "HTTP_X_FORWARDED_FOR" in request.META:
+            ip = request.META["HTTP_X_FORWARDED_FOR"]
+        elif "Client-IP" in request.META:
+            ip = request.META["Client-IP"]
         else:
-            ip = request.META['REMOTE_ADDR']
+            ip = request.META["REMOTE_ADDR"]
         ip = ip.split(",")[0]
         return ip
 
     def process_request(self, request):
-        if not request.user.is_anonymous():
+        if not request.user.is_anonymous:
             ip = self._get_ip(request)
             AuditRequest.new_request(request.get_full_path(), request.user, ip)
         else:
             if settings.DJANGO_SIMPLE_AUDIT_REST_FRAMEWORK_AUTHENTICATOR:
-                user_auth_tuple = settings.DJANGO_SIMPLE_AUDIT_REST_FRAMEWORK_AUTHENTICATOR().authenticate(request)
+                user_auth_tuple = settings.DJANGO_SIMPLE_AUDIT_REST_FRAMEWORK_AUTHENTICATOR().authenticate(
+                    request
+                )
 
                 if user_auth_tuple is not None:
                     user, token = user_auth_tuple
