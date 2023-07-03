4ac57befec4cc11e8e48201ee364a4dd480b01f3
Krzysztof Wrobel <krzysztof.wrobel@goalplan.com>
2023-07-03 15:24:58 +0200

    change migration GPN-2563

--- simple_audit/migrations/0003_alter_request_id.py
+++ simple_audit/migrations/0003_alter_request_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('internal', '0002_auto_20181119_0539'),
+        ('simple_audit', '0002_auto_20181119_0539'),
     ]
 
     operations = [
